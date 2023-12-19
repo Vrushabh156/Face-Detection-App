2ddb2d345f0eed075a3d609346031d6422d01e45
Vrushabh Lewade <vrushabhlewade@gmail.com>
2023-12-19 23:29:46 +0530

    First Commit

--- app/src/main/java/com/ml/facedetectionapp/MainActivity.kt
+++ app/src/main/java/com/ml/facedetectionapp/MainActivity.kt
@@ -1,11 +1,110 @@
 package com.ml.facedetectionapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.ml.facedetectionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val CAMERA_PERMISSION_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCamera.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openCamera()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, 123)
+        } else {
+            Toast.makeText(this, "Oops Something went wrong", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123 && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                detectFace(bitmap)
+            }
+        }
+    }
+
+    private fun detectFace(bitmap: Bitmap) {
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        val detector = FaceDetection.getClient(options)
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                var resultText = " "
+                var i = 1
+                for (face in faces) {
+                    resultText = "Face Number $i" +
+                            "\nSmile : ${face.smilingProbability?.times(100)}%" +
+                            "\nLeft Eye Open : ${face.leftEyeOpenProbability?.times(100)}%" +
+                            "\nRight Eye Open : ${face.rightEyeOpenProbability?.times(100)}%"
+                    i++
+                }
+                if (faces.isEmpty()) {
+                    Toast.makeText(this, "No Face Detected", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, resultText, Toast.LENGTH_LONG).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Something wrong", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
